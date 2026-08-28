b3ca28ed0da734ada3b42fe4a8966b2975976f37
i <isomorphisms@sdf.org>
2026-08-28 10:13:26 -0400

    Add mock Markov story checker

---
+++ experiments/lotka-volterra/check_markov_story_connection.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+cat <<'EOF'
+Boy, I sure wish there were a way to check whether this Markov model actually relates to the deer story.
+
+Right now this is only a mock. Putting the story next to a Markov model does not establish the connection.
+EOF
